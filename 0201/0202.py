48ae56db9c20fe6f414182a51e4263fabe374af0
Eva <ecnolan@hamilton.edu>
2022-12-05 12:54:13 -0500

    finished day 2 p 2

--- 0201/0202.py
+++ 0201/0202.py
@@ -1,23 +1,23 @@
-def get_win_score(opponent, self):
-    if opponent == self:
-        # draw
-        return 3
+def choose_move(round):
+    opponent = ord(round[0]) - 64
+    win_score = (ord(round[2]) - 88) * 3
+    scores = [3, 1, 2, 3, 1]
 
-    if self == opponent + 1 or self == opponent - 2:
-        # self wins
-        return 6
+    if win_score == 3:
+        self_score = opponent
+        return (opponent, win_score)
 
-    # self looses
-    return 0
+    if win_score == 6:
+        pass
+        return (scores[opponent + 1], win_score)
+
+    return (scores[opponent - 1], win_score)
+
 
 def get_round_score(round):
-    opponent = ord(round[0]) - 64
-    self = ord(round[2]) - 87
-
-    win_score = get_win_score(opponent, self)
-    self_score = self
-    # print(self_score, win_score)
+    (self_score, win_score) = choose_move(round)
     return self_score + win_score
+
 
 def get_tournament_score(tournament):
     total_score = 0
