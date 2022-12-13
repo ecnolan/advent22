e39d673261b3939629d1b8dd0d3df296b45ac265
Eva <ecnolan@hamilton.edu>
2022-12-13 07:55:06 -0500

    finished day 9 p 2

--- day09/093.py
+++ day09/093.py
@@ -34,6 +34,20 @@ def updatetail(hpos, tpos):
         return (hr, mean([tc, hc]))
     if abs(hc-tc)==1 and abs(hr-tr)==2:
         return (mean([tr, hr]), hc)
+    if abs(hc-tc)==2 and abs(hr-tr)==2:
+        # added for part 2 possibilities
+        return (mean([tr, hr]), mean([tc, hc]))
+
+
+def strip(list):
+    """ removes consecutive duplicates from list """
+    new = [list[0]]
+    while len(list) != 0:
+        if list[0] != new[-1]:
+            new.append(list[0])
+        list = list[1:]
+
+    return new
 
 
 def main():
@@ -41,37 +55,35 @@ def main():
         lines = f.readlines()
 
     hpos = (0,0)
-
     visited = [(0,0)]
     allheads = []
 
     for line in lines:
         moves = int(line[line.find(' ')+1:-1])
         dir = line[0]
-        # print(moves, dir)
+        print(moves, dir)
         for move in range(moves):
             hpos = updatehead(hpos, dir)
             allheads.append(hpos)
 
-    curr_knot_locations = []
-    prev_knot_locations = allheads
-    for i in range(10):
-        tpos = (0,0) # reset starting location for new knot
-        for hpos in prev_knot_locations:
-            tpos = updatetail(hpos, tpos)
-            curr_knot_locations.append(tpos)
-        # prepare for new knot
-        prev_knot_locations = curr_knot_locations
-        curr_knot_locations = []
-
-    # print(visited)
-    # print(len(visited))
-    stripped_visited = [*set(curr_knot_locations)]
+    stripped_visited = [*set(allheads)]
     print(len(stripped_visited))
 
-
-
-
+    prev_moves = allheads
+    curr_moves = []
+    tpos = (0,0)
+    visited = []
+    for i in range(9):
+        for move in prev_moves:
+            tpos = updatetail(move, tpos)
+            curr_moves.append(tpos)
+            if tpos not in visited:
+                visited.append(tpos)
+        print(len(visited))
+        prev_moves = curr_moves
+        curr_moves = []
+        tpos = (0,0)
+        visited = []
 
 
 if __name__ == '__main__':
